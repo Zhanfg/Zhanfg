05697bee52317ae73c1922ed0af9359b7d744555
Axymorrsen <Axymosen@outlook.com>
2026-08-29 14:31:56 +0800

    feat: scaffold MFGA Xposed Smart v1.4

---
+++ mfga-xposed-smart/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://artifactory.appodeal.com/appodeal-public/")
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "MFGA-Xposed-Smart"
+include(":app")
