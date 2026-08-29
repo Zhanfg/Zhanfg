d5a6a206347122396a4fb1604b2bb5677d6c5526
Axymorrsen <Axymosen@outlook.com>
2026-08-29 14:32:16 +0800

    build: configure MFGA Xposed Smart app

---
+++ mfga-xposed-smart/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mfga.xposed.smart"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.mfga.xposed.smart"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 14
+        versionName = "1.4-smart"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+    sourceSets["main"].resources.srcDirs("src/main/resources")
+}
+
+dependencies {
+    compileOnly("de.robv.android.xposed:api:82")
+    compileOnly("io.github.libxposed:api:102.0.0")
+}
